536989108b138550ee2720edac537b571f6a06d0
jose-cuevas <jgutifer@gmail.com>
2023-08-01 10:17:53 +0100

    read notes works

--- 01_comments.php
+++ 01_comments.php
@@ -8,3 +8,23 @@ Multiline comment
 with some
 long text
 */
+
+
+// $person = [
+//     'name' => 'Brad',
+//     'surname' => 'Traversy',
+//     'age' => 30,
+//     'hobbies' => ['Tennis', 'Video Games'],
+// ];
+
+// foreach($person as $key=>$value){
+//     echo "<pre>";
+//     var_dump ($key, $value);
+//     echo "</pre>";
+// }
+
+// // $arr = array(1, 2, 3, 4);
+
+// // foreach($arr as $key=>$num){
+// //    echo "<p>Key: " .$key. " and Number: " .$num. "</p>";
+// // }
--- 07_loops.php
+++ 07_loops.php
@@ -37,15 +37,24 @@ foreach ($fruits as $i => $fruit) {
 }
 
 // 6. Iterate Over associative array.
-$person = [
-    'name' => 'Brad',
-    'surname' => 'Traversy',
-    'age' => 30,
-    'hobbies' => ['Tennis', 'Video Games'],
-];
-foreach ($person as $key => $value) {
-    if ($key === 'hobbies') { // 8. Break when $key = 'hobbies'
-        break;
-    }
-    echo $key . ' ' . $value . '<br>';
-}
+// $person = [
+//     'name' => 'Brad',
+//     'surname' => 'Traversy',
+//     'age' => 30,
+//     'hobbies' => ['Tennis', 'Video Games'],
+// ];
+// foreach ($person as $key => $value) {
+//     if ($key === 'hobbies') { // 8. Break when $key = 'hobbies'
+//         break;
+//     }
+//     echo $key . ' ' . $value . '<br>';
+// }
+
+
+
+
+
+
+
+
+
